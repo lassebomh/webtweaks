e22a76bb89a34068d9c59048325ece096b1867a9
Lasse H. Bomholt <lasse@bomh.net>
2024-09-03 09:32:45 +0200

    disable controls for localhost

--- controls.js
+++ controls.js
@@ -1,22 +1,22 @@
+if (window.location.host !== "localhost") {
+  document.addEventListener("keydown", (e) => {
+    if (document.activeElement !== document.body) return;
+    if (e.altKey || e.ctrlKey || e.metaKey || e.shiftKey) return;
 
-document.addEventListener('keydown', (e) => {
-
-    if (document.activeElement !== document.body) return
-    if ((e.altKey || e.ctrlKey || e.metaKey || e.shiftKey)) return
-
-    if (e.key == 'j' || e.key == 'k' || e.key == 's' || e.key == 'd') {
-        e.preventDefault();
-        const direction = (e.key == 'j' || e.key == 's') ? 1 : -1
-        const distance = e.repeat ? 150 : 300
-        window.scrollBy({
-            top: distance * direction,
-            behavior: e.repeat ? "instant" : "smooth"
-        });
-    } else if (e.key == 'h' || e.key == 'a') {
-        e.preventDefault();   
-        history.back();
-    } else if (e.key == 'l' || e.key == 'f') {
-        e.preventDefault();   
-        history.forward();
+    if (e.key == "j" || e.key == "k" || e.key == "s" || e.key == "d") {
+      e.preventDefault();
+      const direction = e.key == "j" || e.key == "s" ? 1 : -1;
+      const distance = e.repeat ? 150 : 300;
+      window.scrollBy({
+        top: distance * direction,
+        behavior: e.repeat ? "instant" : "smooth",
+      });
+    } else if (e.key == "h" || e.key == "a") {
+      e.preventDefault();
+      history.back();
+    } else if (e.key == "l" || e.key == "f") {
+      e.preventDefault();
+      history.forward();
     }
-})
+  });
+}
